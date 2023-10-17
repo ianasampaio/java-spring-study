d26990953dd7d514d105a395bec182f3e5d28557
Iana Sampaio <iana.sampaio06@gmail.com>
2023-10-17 11:35:47 -0300

    first commit

---
+++ src/test/java/javaweb/example/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package javaweb.example.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
